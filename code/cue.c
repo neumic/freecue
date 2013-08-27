a11e4f1176274b1352c711ec7f7acca2c6f2c70e
Nick McLarnan <nickgeek@gmail.com>
2013-08-27 19:42:10 -0400

    commiting beforetrying again from scratch

--- code/cue.c
+++ code/cue.c
@@ -1,41 +1,17 @@
-#include <gst/gst.h>
-#include <stdlib.h>
+#include "cue.h"
 
-GstElement* pipeline_array [100];
-int pipeline_array_size = 0;
-
-void add_pipeline( char* uri ){
+void add_pipeline( char* uri, GstElement* pipeline_array[], int* pipeline_array_size );
    GstElement* pipeline = gst_pipeline_new( "audio-player" );
    GstElement* player  = gst_element_factory_make( "playbin", "player" );
    g_object_set( G_OBJECT( player ), "uri", uri, NULL );
    gst_bin_add( GST_BIN( pipeline ), player );
    pipeline_array[pipeline_array_size] = pipeline;
-   pipeline_array_size++;
+   pipeline_array_size&++;
 }
 
-struct Cue {
-   GstElement* pipeline;
-   GstState cueState;
-   struct Cue* nextCue;
-};
-
-int main( int argc, char* argv[] ){
-   GMainLoop* loop;
-
-   gst_init( &argc, &argv );
-
-   loop = g_main_loop_new( NULL, FALSE );
-
-   add_pipeline( "file:///home/nickm/projects/opencue/Hurricane_Connie_1955.ogg" );
-   add_pipeline( "file:///home/nickm/projects/opencue/Hurricane_Connie_1955.ogg" );
-
-   gst_element_set_state( pipeline_array[0], GST_STATE_PLAYING );
-
-   g_print( "hurderp" );
-   g_main_loop_run( loop );
-   gst_element_set_state( pipeline_array[1], GST_STATE_NULL );
-
-   return 0;
+void run_cue( struct Cue cue ){
+   gst_element_set_state( cue.pipeline, cue.state );
 }
+
 
 
