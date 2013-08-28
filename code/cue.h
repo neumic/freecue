0a95b8625fa5a826587bad28258bcbf1a0497631
Nick McLarnan <nickgeek@gmail.com>
2013-08-28 01:43:29 -0400

    header for Cue objects

---
+++ code/cue.h
@@ -0,0 +1,18 @@
+#include <gst/gst.h>
+
+struct Cue {
+   GstElement* media_pipeline;
+   GstState media_state;
+}
+
+struct Cue* Cue_create( GstElement*, GstState );
+
+void Cue_destroy( struct Cue* );
+
+void Cue_print( struct Cue* );
+
+void Cue_set_state( struct Cue*, GstState );
+
+void Cue_set_media( struct Cue*, GstElement* );
+
+void Cue_run( struct Cue* );
