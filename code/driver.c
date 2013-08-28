1dd8f3908d184421788050bc4c48bac1620071f3
Nick McLarnan <nickgeek@gmail.com>
2013-08-28 02:46:46 -0400

    added driver program and modified makefile to include.  Compiles, run, but produces no output

---
+++ code/driver.c
@@ -0,0 +1,33 @@
+#include <gst/gst.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include "cue.h"
+
+int main( int argc, char* argv[] ){
+  GMainLoop *loop;
+  GstElement *media;
+
+  gst_init( &argc, &argv );
+  loop = g_main_loop_new( NULL, FALSE );
+
+  media = gst_element_factory_make( "playbin", "play" );
+  g_object_set( G_OBJECT( media ), "uri", 
+      "file://home/nickm/projects/opencue/Hurricane_Connie_1955.ogg", NULL);
+
+  struct Cue* cue = Cue_create( media, GST_STATE_PLAYING );
+
+  Cue_print( cue );
+
+  Cue_run( cue );
+
+  g_main_loop_run( loop );
+
+  Cue_destroy( cue );
+
+  gst_object_unref( GST_OBJECT( media ) );
+
+  return 0;
+}
+
+
