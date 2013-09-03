eb747c6c9978a6b4141da2939be76d906bf416ca
Nick McLarnan <nickgeek@gmail.com>
2013-09-02 20:55:13 -0400

    added .gitignore and cleaned out some unneeded code

--- py-code/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from gi.repository import Gst
-
-Gst.init_check(None)
-# Playbin is a simple element which plays video and audio in many codecs
-player = Gst.ElementFactory.make("playbin", "player")
-player.set_property("uri", "file:///home/nickm/projects/opencue/Big\ Max\ and\ Jungle\ Jacob.m4a")
-#player.set_property("uri", "file:///home/nickm/projects/opencue/Hurricane_Connie_1955.ogg")
-player.set_state(Gst.State.PLAYING)
